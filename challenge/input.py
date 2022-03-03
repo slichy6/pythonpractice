ba09a1fee698680eefa6a7f97f95d1290499132a
Sarah Lichy <sarahlichy@users.noreply.github.com>
2022-03-03 17:08:00 +0000

    join and split

---
+++ challenge/input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+
+    
+    user_input = input("Please enter your favorite animal:")
+    
+    print("Your favorite animal is: " + user_input)
+    
+main()
+
