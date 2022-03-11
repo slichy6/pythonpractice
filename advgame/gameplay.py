a850b6d75a75c6b4716e17b652feeaa175d71b01
Sarah Lichy <sarah.lichy6@gmail.com>
2022-03-11 04:33:06 +0000

    final tweaks for spacing and player name

--- advgame/gameplay.py
+++ advgame/gameplay.py
@@ -25,7 +25,7 @@ def start_game():
         
             1. Tear down the wall to see what it is
             2. Say not today and leave it as is''')
-            lvl1_choice = input("Enter option number: ")
+            lvl1_choice = input("\n Enter option number: ")
 
             #deciding to tear down the wall
             if lvl1_choice == '1':
@@ -33,14 +33,14 @@ def start_game():
                 lvl2_choice = ""
                 while lvl2_choice not in lvl2_options:
                     print('''
-                You decide to tear down the wall and satisfy your curiosity,
-                once it's open you see it all looks empty at first glance.  Until you look 
-                down and notice a small box shoved in the bottom corner covered in dust.
-                What would you like to do next? 
+            You decide to tear down the wall and satisfy your curiosity,
+            once it's open you see it all looks empty at first glance.  Until you look 
+            down and notice a small box shoved in the bottom corner covered in dust.
+            What would you like to do next? 
                 
-                1. Take the box out to inspect
-                2. Close the wall back up''')
-                    lvl2_choice = input('Enter option number: ')
+            1. Take the box out to inspect
+            2. Close the wall back up''')
+                    lvl2_choice = input('\n Enter option number: ')
                     
                     #taking the box out
                     if lvl2_choice == "1":
@@ -48,15 +48,15 @@ def start_game():
                         lvl3_choice = ""
                         while lvl3_choice not in lvl3_options:
                             print('''
-                You decided to take out the box.  It looks really old and 
-                as you brush the dust off you see it is locked and appears to have
-                ritual symbols and something in latin engraved around the sides.
-                What would you like to do next? 
+            You decided to take out the box.  It looks really old and 
+            as you brush the dust off you see it is locked and appears to have
+            ritual symbols and something in latin engraved around the sides.
+            What would you like to do next? 
                 
-                1. Search the house to find the key
-                2. Throw it away
-                3. Call for spiritual help''')
-                            lvl3_choice = input('Enter option number: ')
+            1. Search the house to find the key
+            2. Throw it away
+            3. Call for spiritual help''')
+                            lvl3_choice = input('\n Enter option number: ')
                         
                         #option 1: search for the key
                         if lvl3_choice == "1":
@@ -64,21 +64,21 @@ def start_game():
                             lvl4_choice = ""
                             while lvl4_choice not in lvl4_options:
                                 print('''
-                You decided to look around to see if you can find the key, 
-                you can't find it on the ground floor. Would you like to keep trying
-                to find the key or get rid of the box?
+            You decided to look around to see if you can find the key, 
+            you can't find it on the ground floor. Would you like to keep trying
+            to find the key or get rid of the box?
                             
-                1. look in the attic for the key
-                2. throw the box away''')
-                                lvl4_choice = input('Enter option number: ')
+            1. look in the attic for the key
+            2. throw the box away''')
+                                lvl4_choice = input('\n Enter option number: ')
 
                                 #option 1: keep looking
                                 if lvl4_choice == "1":
-                                    print("you decided to keep looking, days later you still don't find it, YOU LOSE!")
+                                    print(f"you decided to keep looking, days later you still don't find it, YOU LOSE {name}!")
                                 
                                 #option 2: throw the box away
-                            else:
-                                print("You decided to throw away the box and hope you don't find anything else in the house, you survive!")
+                                else:
+                                    print("You decided to throw away the box and hope you don't find anything else in the house, you survive!")
 
 
                         #option 2: Throw it away
@@ -87,16 +87,16 @@ def start_game():
 
                         #option 3: call for spiritual help
                         else:
-                            print('''
-                 You decided to call for help to eradicate the spirits in
-                 your home, now you get to keep your beautiful home without the 
-                 spirits, YOU WIN! ''')    
+                            print(f'''
+            You decided to call for help to eradicate the spirits in
+            your home, now you get to keep your beautiful house without the 
+            spirits, CONGRATULATIONS {name} YOU WIN!''')    
 
                     #close the wall back up
                     else:
                         print('''
-                 You close the wall back up and think you avoided a potentially horrible outcome, GAME OVER!
-                 For now.....''')
+            You close the wall back up and think you avoided a potentially horrible outcome, GAME OVER!
+            For now.....''')
                             
             #not today and leave it alone
             else:
@@ -104,6 +104,6 @@ def start_game():
 
 #goodbye message if they enter no
     else:
-        print("That's too bad, maybe next time")
+        print(f"That's too bad, maybe next time {name}")
 
 start_game() 
